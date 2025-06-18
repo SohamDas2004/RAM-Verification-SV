9cd5b5de5c6f1bf30c25589d14a1b8964f4dae5d
AstroGan <161059759+SohamDas2004@users.noreply.github.com>
2025-06-18 15:40:24 +0530

    Create design.sv

---
+++ design.sv
@@ -0,0 +1,26 @@
+module ram_rtl(input clk,rst,rd_enb,wr_enb,
+ 
+ 				input [3:0] wr_addr,rd_addr,
+ 
+ 				input [31:0] data_in,
+ 
+ 				output reg [31:0] data_out);
+ 
+ 
+ reg [31:0] mem [7:0]; //total location : 8,each location width = 32;
+ 
+ //write operation
+ 
+ always@(posedge clk) 
+ begin
+ if(wr_enb)
+ mem[wr_addr] <= data_in;
+ 
+ end
+ 
+ always@(posedge clk)
+ begin
+ if(rd_enb)
+ data_out <= mem[rd_addr];
+ end
+endmodule
